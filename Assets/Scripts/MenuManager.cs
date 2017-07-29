a54d6872e95be44fd927e4ae8c7fc465289d1b0f
Jipay <jipay.detiege@gmail.com>
2017-07-29 13:31:47 +0200

    Add menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public PanelInfoManager PanelInfo;
+
+    void Awake()
+    {
+        PanelInfo.Hide();
+    }
+
+    public void Play()
+    {
+        // todo
+    }
+
+    public void DisplayRules()
+    {
+        PanelInfo.Display("RULES");
+    }
+
+    public void DisplayOptions()
+    {
+        PanelInfo.Display("OPTIONS");
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
---
+++ Assets/Scripts/PanelInfoManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PanelInfoManager : MonoBehaviour
+{
+    public Text Title;
+    public GameObject Panel;
+
+    public void Display(string title)
+    {
+        Title.text = title;
+
+        Panel.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        Panel.SetActive(false);
+    }
+}
