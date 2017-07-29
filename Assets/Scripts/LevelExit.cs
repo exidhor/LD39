9dd941d9ae7ab39d04f2b60ff2384f19b39eb9fa
Celtixtime <Celtixtime@gmail.com>
2017-07-29 18:58:12 +0200

    ajout du script de la porte, ToDo ajouter les transitions entre la taverne et les niveaux

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelExit : MonoBehaviour {
+
+    private float level;
+    private bool iscavern;
+
+    void Start()
+    {
+        iscavern = true;
+        level = 0;
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Player player = collider.GetComponent<Player>();
+        
+        if (player != null)
+        {
+            if (iscavern)
+            {
+                level++;
+                // transition du joueur vers le niuveau suivant
+            }
+            else
+            {
+                // transition du joueur vers la caverne 
+            }
+            iscavern = !iscavern;
+        }
+
+    }
+}
