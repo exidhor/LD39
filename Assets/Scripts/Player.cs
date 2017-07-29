8e18dbb5ff555b5a6879970c67124b29a81f1f2d
Jipay <jipay.detiege@gmail.com>
2017-07-29 12:01:35 +0200

    Add deplacement and jump

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed;
+    public float JumpSpeed;
+    public float Gravity;
+
+    private CharacterController _controller;
+    private Vector3 _movement;
+
+    void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (_controller.isGrounded)
+        {
+            // reset movement
+            _movement = Vector3.zero;
+
+            // Handle jump
+            if (Input.GetButtonDown("Jump"))
+            {
+                _movement.y = JumpSpeed;
+            }
+        }
+
+        // move verticaly
+        _movement.x = Input.GetAxisRaw("Horizontal") * Speed;
+
+        // Handle gravity
+        _movement.y -= Gravity * Time.deltaTime;
+
+        // Move the character
+        Move();
+    }
+
+    void Move()
+    {
+        _controller.Move(_movement * Time.deltaTime);
+    }
+}
